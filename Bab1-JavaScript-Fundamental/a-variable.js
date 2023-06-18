b4b6e0f90e641f7a88c4de1b22ab3232320a099f
Dandi Lesmana <dandilesmana.if21@gmail.com>
2023-06-18 13:10:17 +0700

    update

---
+++ Bab1-JavaScript-Fundamental/a-variable.js
@@ -0,0 +1,18 @@
+
+//variable di javaScript di deklarasikan menggunakan kata kunci:
+//  ===>> var, let, const 
+
+
+let nama = 'ucup';
+var umur = '21';
+const tahunLahir = 2000
+
+
+
+
+console.log(nama);
+console.log(umur);
+console.log(tahunLahir);
+
+
+console.log(`nama saya ${nama}. usia saya ${umur} tahun. saya lahir di tahun ${tahunLahir}.`);
